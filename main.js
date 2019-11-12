6f0e666c411cae160542fbd8387310ea38ecc6c9
Emin Ter-Mkrtchyan <eminter-mkrtchyan@Emins-MacBook-Air.local>
2019-11-12 18:12:42 +0400

    prints the primes of the range

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const util = require("./myUtils.js");
 
-console.log(util.isPrime(2));
+const reader = require("readline-sync");
+let input1 = reader.questionInt("Input any positive number ");
+let input2 = reader.questionInt("Input another positive number ");
+
+console.log("Here are all the prime numbers between " + input1 + " and " + input2);
+util.intoRangeOfPrimes(input1,input2);
 
--- myUtils.js
+++ myUtils.js
@@ -3,17 +3,38 @@ module.exports = {
 		if(n <=1) {
 			return false;
 		}
-
 		let div = 2;
-
-		while(div <= Math.sqrt(n)) {
+          while(div <= Math.sqrt(n)) {
 			if(n % div === 0) {
 				return false;
 			}
-
-			div++;
+           div++;
 		}
-
-		return true;
+      return true;
+	},
+	intoRangeOfPrimes: function(int1,int2){
+		let rangeOfInputs = [];
+		let rangeOfPrimes = [];
+		let max;
+		let min;
+		if(int1>int2){
+			max = int1;
+			min = int2;
+		}
+		else{
+			max = int2;
+			min = int1;
+		}
+		while(max>=min){
+			rangeOfInputs.push(min);
+			min++;
+		}
+	
+		for(let i=0; i<rangeOfInputs.length; i++){
+			if(this.isPrime(rangeOfInputs[i])){
+			   rangeOfPrimes.push(rangeOfInputs[i]);
+			}
+		}
+	  console.log(rangeOfPrimes);
 	}
 }
